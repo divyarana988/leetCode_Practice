74cc650fcd483d0ed6765628b5f079b0c7027307
DIVYA RANA <divyarana491@gmail.com>
2021-08-11 10:17:27 +0530

    added files

---
+++ tree/invertBT.java
@@ -0,0 +1,26 @@
+class Solution {
+   public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
