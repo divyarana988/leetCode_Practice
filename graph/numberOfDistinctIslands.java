01e7970c7fe1202910cce3d53557080dbb61876b
DIVYA RANA <divyarana491@gmail.com>
2021-08-29 00:08:06 +0530

    added files

---
+++ graph/numberOfDistinctIslands.java
@@ -0,0 +1,38 @@
+ public int numberofDistinctIslands(int[][] grid) {
+        // write your code here
+        HashSet<String> set = new HashSet<>();
+        StringBuilder psf;
+
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
+
+                if(grid[r][c]==1){
+                    psf= new StringBuilder("X");
+                    helper(grid, r, c, psf);
+                    set.add(psf.toString());
+                }
+            }
+        }
+        return set.size();
+    }
+    public void helper(int[][]grid, int r, int c, StringBuilder psf){
+        grid[r][c] = 2;
+
+        if(r-1>=0 && grid[r-1][c]==1){
+            psf.append("N");
+            helper(grid, r-1, c, psf);
+        }
+        if(c+1<grid[0].length && grid[r][c+1]==1){
+            psf.append("E");
+            helper(grid, r, c+1, psf);
+        }
+        if(r+1<grid.length && grid[r+1][c]==1){
+            psf.append("S");
+            helper(grid, r+1, c, psf);
+        }
+        if(c-1>=0 && grid[r][c-1]==1){
+            psf.append("N");
+            helper(grid, r, c-1, psf);
+        }
+        psf.append("Z");
+    }
---
+++ graph/rottingOranges.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair> que = new ArrayDeque<>();
+        int nFreshORanges = 0;
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
+                if(grid[r][c]==1){
+                    nFreshORanges++;
+                }else if(grid[r][c]==2)
+                    que.add(new Pair(r,c));
+            }
+        }
+        int dir[][] = {{-1, 0}, {0, +1}, {+1, 0}, {0, -1}};
+        int level = 0;
+        while(que.size()>0){
+            int n = que.size();
+            while(n-->0){
+                Pair rem = que.remove();
+                for(int d=0; d<4; d++){
+                    int rd = rem.r + dir[d][0];
+                    int cd = rem.c + dir[d][1];
+                    
+                    if(rd<0 || cd<0 || rd>=grid.length || cd>=grid[0].length || grid[rd][cd] != 1)
+                        continue;
+                    
+                    nFreshORanges--;
+                    grid[rd][cd] = 2;
+                    que.add(new Pair(rd, cd));
+                }
+            }
+            if(que.size()!=0) level++;
+        }
+        if(nFreshORanges==0)
+            return level;
+        else 
+            return -1;
+    }
+    public class Pair{
+        int r, c;
+        Pair(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+}
