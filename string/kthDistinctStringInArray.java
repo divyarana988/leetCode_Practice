8c6498b1bbce44432b0db670eac702305d9a70c5
DIVYA RANA <divyarana491@gmail.com>
2021-11-05 23:29:47 +0530

    added files

---
+++ string/kthDistinctStringInArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) 
+        { 
+            if (hm.containsKey(arr[i]))  
+            { 
+                hm.put(arr[i], hm.get(arr[i]) + 1); 
+            }  
+            else
+            { 
+                hm.put(arr[i], 1); 
+            } 
+        } 
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+            if(hm.get(arr[i])==1){
+                count++;
+                if(count==k){
+                    return arr[i];
+                }
+            }
+            
+        }
+        return "";
+    }
+}
